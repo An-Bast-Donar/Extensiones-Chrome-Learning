71b291c32930e94338aefe4b9e9ec3e5ec7f65f6
An Bast <sebastianrondonja@hotmail.com>
2023-05-14 00:25:02 -0500

    Learning
    - Leer pestañas
    - Imprimir en consola
    - Mostrar notificaciones
    - Acceso a la consola de la extension

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 Aprendiendo lo basico para hacer extensiones para google chrome
 
-
 # Intro
 
 - Se programan en js
@@ -10,3 +9,4 @@ Aprendiendo lo basico para hacer extensiones para google chrome
 - Cada manifiesto de chrome tiene fecha de vigencia, por ejemplo el 3, va del 2023 al 2025 mas o menos
 - Se reqquiere crear un manifest.json que contiene las caracteristicas de la extension
 - Cargamos la extension a goole
+- Para poder ir revisando cambios, puedes imprimir en consola datos, pero esta consola no es la del navegador si no la de la extension, puedes abrir esta consolas en la opcion inspect views dentro de los detalles de la extension
--- background.js
+++ background.js
@@ -1,3 +1,24 @@
+chrome.tabs.onCreated.addListener(function (tab) {
+  chrome.tabs.get(tab.id, function (tabInfo) {
+    var notificationOptions = {
+      type: "basic",
+      iconUrl: "icon.png",
+      title: "Nueva pestaña creada",
+      message: tabInfo.title,
+    };
+
+    chrome.notifications.create(notificationOptions);
+  });
+
+  chrome.tabs.query({}, function (tabs) {
+    var tabList = tabs.map(function (tab) {
+      return tab.title;
+    });
+
+    console.log("Pestañas existentes:", tabList);
+  });
+});
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === "complete" && /^https/.test(tab.url)) {
     chrome.scripting
@@ -8,6 +29,8 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
       .then(() => {
         console.log("INJECTED THE FOREGROUND SCRIPT.");
       })
-      .catch((err) => {console.log("error:", err)});
+      .catch((err) => {
+        console.log("error:", err);
+      });
   }
 });
